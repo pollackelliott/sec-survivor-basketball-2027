ba06d90fe7f1e5256b47008e6d3ad854f9e10bbc
Elliott Pollack <pollack.elliott@gmail.com>
2026-07-27 21:30:55 -0500

    Create sec.js

---
+++ config/conferences/sec.js
@@ -0,0 +1,28 @@
+window.CONFERENCE_MEMBERSHIPS = window.CONFERENCE_MEMBERSHIPS || {};
+
+window.CONFERENCE_MEMBERSHIPS['sec-2025'] = {
+  id: 'sec-2025',
+  conferenceId: 'sec',
+  conferenceName: 'SEC',
+  effectiveFrom: 2025,
+  effectiveThrough: null,
+
+  teamIds: [
+    'alabama',
+    'arkansas',
+    'auburn',
+    'florida',
+    'georgia',
+    'kentucky',
+    'lsu',
+    'mississippi-state',
+    'missouri',
+    'oklahoma',
+    'ole-miss',
+    'south-carolina',
+    'tennessee',
+    'texas',
+    'texas-am',
+    'vanderbilt'
+  ]
+};
