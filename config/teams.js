0f2d89ca25fff0907b09cb28d4e63ca368ec67bd
Elliott Pollack <pollack.elliott@gmail.com>
2026-07-27 21:26:08 -0500

    Create teams.js

---
+++ config/teams.js
@@ -0,0 +1,307 @@
+window.TEAM_REGISTRY = {
+  'alabama': {
+    id: 'alabama',
+    schoolName: 'Alabama',
+    displayName: 'Alabama',
+    abbreviation: 'ALA',
+    primaryColor: '#9E1B32',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'arkansas': {
+    id: 'arkansas',
+    schoolName: 'Arkansas',
+    displayName: 'Arkansas',
+    abbreviation: 'ARK',
+    primaryColor: '#9D2235',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'auburn': {
+    id: 'auburn',
+    schoolName: 'Auburn',
+    displayName: 'Auburn',
+    abbreviation: 'AUB',
+    primaryColor: '#0C2340',
+    secondaryColor: '#E87722'
+  },
+
+  'florida': {
+    id: 'florida',
+    schoolName: 'Florida',
+    displayName: 'Florida',
+    abbreviation: 'UF',
+    primaryColor: '#0021A5',
+    secondaryColor: '#FA4616'
+  },
+
+  'georgia': {
+    id: 'georgia',
+    schoolName: 'Georgia',
+    displayName: 'Georgia',
+    abbreviation: 'UGA',
+    primaryColor: '#BA0C2F',
+    secondaryColor: '#000000'
+  },
+
+  'kentucky': {
+    id: 'kentucky',
+    schoolName: 'Kentucky',
+    displayName: 'Kentucky',
+    abbreviation: 'UK',
+    primaryColor: '#0033A0',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'lsu': {
+    id: 'lsu',
+    schoolName: 'LSU',
+    displayName: 'LSU',
+    abbreviation: 'LSU',
+    primaryColor: '#461D7C',
+    secondaryColor: '#FDD023'
+  },
+
+  'mississippi-state': {
+    id: 'mississippi-state',
+    schoolName: 'Mississippi State',
+    displayName: 'Miss. State',
+    abbreviation: 'MSU',
+    primaryColor: '#660000',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'missouri': {
+    id: 'missouri',
+    schoolName: 'Missouri',
+    displayName: 'Missouri',
+    abbreviation: 'MIZ',
+    primaryColor: '#000000',
+    secondaryColor: '#F1B82D'
+  },
+
+  'oklahoma': {
+    id: 'oklahoma',
+    schoolName: 'Oklahoma',
+    displayName: 'Oklahoma',
+    abbreviation: 'OU',
+    primaryColor: '#841617',
+    secondaryColor: '#FDF9D8'
+  },
+
+  'ole-miss': {
+    id: 'ole-miss',
+    schoolName: 'Ole Miss',
+    displayName: 'Ole Miss',
+    abbreviation: 'MISS',
+    primaryColor: '#14213D',
+    secondaryColor: '#CE1126'
+  },
+
+  'south-carolina': {
+    id: 'south-carolina',
+    schoolName: 'South Carolina',
+    displayName: 'S. Carolina',
+    abbreviation: 'USC',
+    primaryColor: '#73000A',
+    secondaryColor: '#000000'
+  },
+
+  'tennessee': {
+    id: 'tennessee',
+    schoolName: 'Tennessee',
+    displayName: 'Tennessee',
+    abbreviation: 'TENN',
+    primaryColor: '#FF8200',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'texas': {
+    id: 'texas',
+    schoolName: 'Texas',
+    displayName: 'Texas',
+    abbreviation: 'TEX',
+    primaryColor: '#BF5700',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'texas-am': {
+    id: 'texas-am',
+    schoolName: 'Texas A&M',
+    displayName: 'Texas A&M',
+    abbreviation: 'A&M',
+    primaryColor: '#500000',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'vanderbilt': {
+    id: 'vanderbilt',
+    schoolName: 'Vanderbilt',
+    displayName: 'Vanderbilt',
+    abbreviation: 'VU',
+    primaryColor: '#866D4B',
+    secondaryColor: '#000000'
+  },
+
+  'illinois': {
+    id: 'illinois',
+    schoolName: 'Illinois',
+    displayName: 'Illinois',
+    abbreviation: 'ILL',
+    primaryColor: '#E84A27',
+    secondaryColor: '#13294B'
+  },
+
+  'indiana': {
+    id: 'indiana',
+    schoolName: 'Indiana',
+    displayName: 'Indiana',
+    abbreviation: 'IU',
+    primaryColor: '#960009',
+    secondaryColor: '#EEEDEB'
+  },
+
+  'iowa': {
+    id: 'iowa',
+    schoolName: 'Iowa',
+    displayName: 'Iowa',
+    abbreviation: 'IOWA',
+    primaryColor: '#FFCD00',
+    secondaryColor: '#000000'
+  },
+
+  'maryland': {
+    id: 'maryland',
+    schoolName: 'Maryland',
+    displayName: 'Maryland',
+    abbreviation: 'UMD',
+    primaryColor: '#E03A3E',
+    secondaryColor: '#FFD520'
+  },
+
+  'michigan': {
+    id: 'michigan',
+    schoolName: 'Michigan',
+    displayName: 'Michigan',
+    abbreviation: 'MICH',
+    primaryColor: '#00274C',
+    secondaryColor: '#FFCB05'
+  },
+
+  'michigan-state': {
+    id: 'michigan-state',
+    schoolName: 'Michigan State',
+    displayName: 'Michigan State',
+    abbreviation: 'MSU',
+    primaryColor: '#18453B',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'minnesota': {
+    id: 'minnesota',
+    schoolName: 'Minnesota',
+    displayName: 'Minnesota',
+    abbreviation: 'MINN',
+    primaryColor: '#7A0019',
+    secondaryColor: '#FFCC33'
+  },
+
+  'nebraska': {
+    id: 'nebraska',
+    schoolName: 'Nebraska',
+    displayName: 'Nebraska',
+    abbreviation: 'NEB',
+    primaryColor: '#E41C38',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'northwestern': {
+    id: 'northwestern',
+    schoolName: 'Northwestern',
+    displayName: 'Northwestern',
+    abbreviation: 'NU',
+    primaryColor: '#4E2A84',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'ohio-state': {
+    id: 'ohio-state',
+    schoolName: 'Ohio State',
+    displayName: 'Ohio State',
+    abbreviation: 'OSU',
+    primaryColor: '#BB0000',
+    secondaryColor: '#B1B7BC'
+  },
+
+  'oregon': {
+    id: 'oregon',
+    schoolName: 'Oregon',
+    displayName: 'Oregon',
+    abbreviation: 'UO',
+    primaryColor: '#154733',
+    secondaryColor: '#FEE123'
+  },
+
+  'penn-state': {
+    id: 'penn-state',
+    schoolName: 'Penn State',
+    displayName: 'Penn State',
+    abbreviation: 'PSU',
+    primaryColor: '#041E42',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'purdue': {
+    id: 'purdue',
+    schoolName: 'Purdue',
+    displayName: 'Purdue',
+    abbreviation: 'PUR',
+    primaryColor: '#CEB888',
+    secondaryColor: '#000000'
+  },
+
+  'rutgers': {
+    id: 'rutgers',
+    schoolName: 'Rutgers',
+    displayName: 'Rutgers',
+    abbreviation: 'RU',
+    primaryColor: '#CC0033',
+    secondaryColor: '#FFFFFF'
+  },
+
+  'ucla': {
+    id: 'ucla',
+    schoolName: 'UCLA',
+    displayName: 'UCLA',
+    abbreviation: 'UCLA',
+    primaryColor: '#2D68C4',
+    secondaryColor: '#F2A900'
+  },
+
+  'usc': {
+    id: 'usc',
+    schoolName: 'USC',
+    displayName: 'USC',
+    abbreviation: 'USC',
+    primaryColor: '#990000',
+    secondaryColor: '#FFC72C'
+  },
+
+  'washington': {
+    id: 'washington',
+    schoolName: 'Washington',
+    displayName: 'Washington',
+    abbreviation: 'WASH',
+    primaryColor: '#4B2E83',
+    secondaryColor: '#B7A57A'
+  },
+
+  'wisconsin': {
+    id: 'wisconsin',
+    schoolName: 'Wisconsin',
+    displayName: 'Wisconsin',
+    abbreviation: 'WISC',
+    primaryColor: '#C5050C',
+    secondaryColor: '#FFFFFF'
+  }
+};
