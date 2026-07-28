0a99a863e0f998c7ff3ca4f26dded9c472957dff
Elliott Pollack <pollack.elliott@gmail.com>
2026-07-27 21:31:16 -0500

    Create big-ten.js

---
+++ config/conferences/big-ten.js
@@ -0,0 +1,30 @@
+window.CONFERENCE_MEMBERSHIPS = window.CONFERENCE_MEMBERSHIPS || {};
+
+window.CONFERENCE_MEMBERSHIPS['big-ten-2025'] = {
+  id: 'big-ten-2025',
+  conferenceId: 'big-ten',
+  conferenceName: 'Big Ten',
+  effectiveFrom: 2025,
+  effectiveThrough: null,
+
+  teamIds: [
+    'illinois',
+    'indiana',
+    'iowa',
+    'maryland',
+    'michigan',
+    'michigan-state',
+    'minnesota',
+    'nebraska',
+    'northwestern',
+    'ohio-state',
+    'oregon',
+    'penn-state',
+    'purdue',
+    'rutgers',
+    'ucla',
+    'usc',
+    'washington',
+    'wisconsin'
+  ]
+};
