8e1eb770db8fcb85a9db8f4604e824c9cf11a2ce
Elliott Pollack <pollack.elliott@gmail.com>
2026-08-03 15:40:26 -0500

    Add buildConferenceConfig function for conference setup

---
+++ config/buildConferenceConfig.js
@@ -0,0 +1,38 @@
+window.buildConferenceConfig = function buildConferenceConfig(membership, teamRegistry){
+  const theme =
+  window.CONFERENCE_THEMES?.[membership.conferenceId];
+
+  if(!theme){
+    throw new Error(
+      `No theme is defined for conference: ${membership.conferenceId}`
+    );
+  }
+
+  const teams = membership.teamIds.map(teamId => teamRegistry[teamId]);
+
+  const config = {
+    id: membership.conferenceId,
+    membershipId: membership.id,
+    name: membership.conferenceName,
+    theme,
+    teamIds: [...membership.teamIds],
+    teams: [],
+    colors: {},
+    secondaryColors: {},
+    abbreviations: {},
+    displayNames: {}
+  };
+
+  teams.forEach(team => {
+    config.teams.push(team.schoolName);
+    config.colors[team.schoolName] = team.primaryColor;
+    config.secondaryColors[team.schoolName] = team.secondaryColor;
+    config.abbreviations[team.schoolName] = team.abbreviation;
+
+    if(team.displayName !== team.schoolName){
+      config.displayNames[team.schoolName] = team.displayName;
+    }
+  });
+
+  return config;
+}
