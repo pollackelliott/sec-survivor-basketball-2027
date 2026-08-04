f2a2dac6541574c5e257691afa789aa3109b890b
Elliott Pollack <pollack.elliott@gmail.com>
2026-08-03 23:13:12 -0500

    Update index.js

--- engine/index.js
+++ engine/index.js
@@ -9,3 +9,21 @@ window.SurvivorEngine.Results = {
     return game.winner === team ? 1 : 0;
   }
 };
+
+window.SurvivorEngine.Schedule = {
+  opponentForGame(game, team){
+    if(!game){
+      return null;
+    }
+
+    if(game.home === team){
+      return game.away;
+    }
+
+    if(game.away === team){
+      return game.home;
+    }
+
+    return null;
+  }
+};
