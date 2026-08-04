3002a6aa9f0d9300ebfac1b8178a71b4183cd7b3
Elliott Pollack <pollack.elliott@gmail.com>
2026-08-04 17:11:38 -0500

    Update football.js

--- config/rulesets/football.js
+++ config/rulesets/football.js
@@ -9,5 +9,19 @@ window.RULESET_REGISTRY.football = Object.freeze({
       nonConference: 3,
       g5: 1
     })
+  }),
+
+  outcomes: Object.freeze({
+    records: Object.freeze({
+      '1-0': Object.freeze({
+        state: 'safe',
+        strikesAdded: 0
+      }),
+
+      '0-1': Object.freeze({
+        state: 'eliminated',
+        strikesAdded: 0
+      })
+    })
   })
 });
