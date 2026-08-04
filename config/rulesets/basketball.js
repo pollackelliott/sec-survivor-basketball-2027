92d99b638361c6943b8ae99bb7af153cf211b85d
Elliott Pollack <pollack.elliott@gmail.com>
2026-08-04 17:11:55 -0500

    Update basketball.js

--- config/rulesets/basketball.js
+++ config/rulesets/basketball.js
@@ -2,5 +2,34 @@ window.RULESET_REGISTRY = window.RULESET_REGISTRY || {};
 
 window.RULESET_REGISTRY.basketball = Object.freeze({
   key: 'basketball',
-  sportKey: 'basketball'
+  sportKey: 'basketball',
+
+  outcomes: Object.freeze({
+    records: Object.freeze({
+      '1-0': Object.freeze({
+        state: 'safe',
+        strikesAdded: 0
+      }),
+
+      '0-1': Object.freeze({
+        state: 'strike',
+        strikesAdded: 1
+      }),
+
+      '2-0': Object.freeze({
+        state: 'safe',
+        strikesAdded: 0
+      }),
+
+      '1-1': Object.freeze({
+        state: 'strike',
+        strikesAdded: 1
+      }),
+
+      '0-2': Object.freeze({
+        state: 'eliminated',
+        strikesAdded: 0
+      })
+    })
+  })
 });
